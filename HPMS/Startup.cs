6b2a43b6ed9abb797b42af264c483bb0d814104b
Kenneth Waiswa <wswkenneth7@hotmail.com>
2019-11-08 00:03:31 +0300

    Add project files.

---
+++ HPMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HPMS.Startup))]
+namespace HPMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
